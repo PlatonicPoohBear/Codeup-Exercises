3f82ff90cdeaff3c83fc41722f03ca8e2f219a67
PlatonicPoohBear <biologynut10@hotmail.com>
2016-03-03 13:34:25 -0600

    Edit list_with_commas.php

--- list_with_commas.php
+++ list_with_commas.php
@@ -3,8 +3,13 @@
 $physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
 
 
-function humanizedList($string) {
+function humanizedList($string, $sortFlag = false) {
 	$array = explode(', ', $string);
+
+	if ($sortFlag === true) {
+		sort($array);
+	}
+
 	$lastIndex = count($array) - 1;
 	$array[$lastIndex] = "and " . $array[$lastIndex];
 	$newString = implode(', ', $array);
@@ -12,7 +17,7 @@ function humanizedList($string) {
 }
 
 
-$famousFakePhysicists = humanizedList($physicistsString);
+$famousFakePhysicists = humanizedList($physicistsString, true);
 
 echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}.";
 
