33bcacade49adbd2a87b9802a0035b43bad3a6ed
PlatonicPoohBear <biologynut10@hotmail.com>
2016-03-09 16:14:20 -0600

    Edit to use constructors and destructors

--- Log.php
+++ Log.php
@@ -3,19 +3,24 @@
 class Log
 {
     public $filename;
+    public $handle;
     
+    
+    public function __construct()
+    {
+    	$this->filename = "Log/log-" . date('y-m-d') . ".log";
+
+    	$this->handle = fopen($this->filename, 'a');
+    }
+
     public function logMessage($logLevel, $message)
 	{
 	    date_default_timezone_set('America/Chicago');
 
 	    $log_entry = date('y-m-d h:i:s') . "[{$logLevel}]" . $message . PHP_EOL;
 
-	    $this->filename = "Log/log-" . date('y-m-d') . ".log";
-	    $handle = fopen($this->filename, 'a');
+	    fwrite($this->handle, $log_entry);
 
-	    fwrite($handle, $log_entry);
-
-	    fclose($handle);
 	}
     
     public function log_error() 
@@ -26,6 +31,11 @@ class Log
     public function log_info()
     {
 		$this->logMessage("INFO", "This is an info message.");
+	}
+
+	public function __destruct()
+	{
+		fclose($this->handle);
 	}
     
 }
--- log_test.php
+++ log_test.php
@@ -5,8 +5,6 @@ require_once 'Log.php';
 
 $test = new Log();
 
-$test->filename = "Log/log-" . date('y-m-d') . ".log";
-
 $test->log_info();
 
 $test->log_error();
